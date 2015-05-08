c95f98a60e5f56874aea367e61ae11bac25bf354
briancanty <brian_canty@mba.berkeley.edu>
2015-05-07 19:47:08 -0700

    Slot Machine App Final

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,17 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Brian Canty on 5/7/15.
+//  Copyright (c) 2015 Brian Canty. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
+
